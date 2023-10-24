141a1994f1a2f3fdf2e978e6dce3d44935d516a1
Matías Gabriel Krujoski <matias.krujoski@fio.unam.edu.ar>
2023-10-24 12:13:54 -0300

    Create lex_sintax.c

---
+++ lex_sintax.c
@@ -0,0 +1,19 @@
+%{
+	// Definiciones
+
+%}
+
+%%
+// Reglas
+
+<patrón1>	{ acción1}
+<patrón2>	{ acción2}
+...
+<patrónN>	{ acciónN}
+%%
+
+// Código auxiliar en lenguaje C
+int main(){
+	...
+	return 0;
+}
