40a69f6693001111b0658b913f6baa3c14e7f764
Matías Gabriel Krujoski <matias.krujoski@fio.unam.edu.ar>
2023-10-24 12:12:31 -0300

    Create ejemplo1.c

---
+++ ejemplo1.c
@@ -0,0 +1,25 @@
+// Declaraciones
+%{
+int count = 0; // variable global del programa en C
+%}
+
+// Reglas
+%%
+  // patrón 1
+[A-Z] {printf("%s es mayúscula\n", yytext);
+      count++;}
+      
+  // patrón 2
+.	 {printf("%s no es mayúscula\n", yytext);}
+
+  // patrón 3 (escape)
+\n {return 0;}
+%%
+
+// Auxiliar
+int yywrap(){} // función especial del compiler lex
+int main(){
+  yylex();
+  printf("\nLa cadena tiene %d letras mayúsculas\n", count);
+return 0;
+}
